00f8606dbebc40a061049b213271d3a4fb3b04e3
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-01-13 12:19:02 +0100

    Added support for Distutils setup on Windows.

--- cefpython/cef3/windows/installer/make-setup.py
+++ cefpython/cef3/windows/installer/make-setup.py
@@ -12,14 +12,38 @@ import re
 import platform
 import shutil
 import glob
+import shutil
 
 BITS = platform.architecture()[0]
 assert (BITS == "32bit" or BITS == "64bit")
+BITS_NUMBER = BITS[0] + BITS[1]
 PACKAGE_NAME = "cefpython3"
 
 README_TEMPLATE = os.getcwd()+r"/README.txt.template"
 INIT_TEMPLATE = os.getcwd()+r"/__init__.py.template"
 SETUP_TEMPLATE = os.getcwd()+r"/setup.py.template"
+
+def glob_remove(pathname):
+    filelist = glob.glob(pathname)
+    for f in filelist:
+        os.remove(f)
+
+def glob_copy(src_glob, dst_folder):
+    for fname in glob.iglob(src_glob):
+        print("Copying %s to %s" % (fname, dst_folder))
+        if os.path.isdir(fname):
+            shutil.copytree(fname,
+                    os.path.join(dst_folder, os.path.basename(fname)))
+        else:
+            shutil.copy(fname,
+                    os.path.join(dst_folder, os.path.basename(fname)))
+
+def glob_move(src_glob, dst_folder):
+    if not os.path.exists(dst_folder):
+        os.mkdir(dst_folder)
+    for fname in glob.iglob(src_glob):
+        shutil.move(fname,
+                os.path.join(dst_folder, os.path.basename(fname)))
 
 def main():
     parser = argparse.ArgumentParser(usage="%(prog)s [options]")
@@ -49,13 +73,15 @@ def main():
 
     installer_dir = os.path.dirname(os.path.abspath(__file__))
 
-    setup_dir = installer_dir+"/"+PACKAGE_NAME+"-"+vars["APP_VERSION"]+"-linux-"+BITS+"-setup"
+    pyVersion = str(sys.version_info.major) + str(sys.version_info.minor)
+    setup_dir = installer_dir+"/"+PACKAGE_NAME+"-"+vars["APP_VERSION"]\
+            +"-win"+BITS_NUMBER+"-py"+pyVersion+"-setup"
     print("Creating setup dir: "+setup_dir)
     os.mkdir(setup_dir)
 
     package_dir = setup_dir+"/"+PACKAGE_NAME
-    print("Creating package dir")
-    os.mkdir(package_dir)
+    #print("Creating package dir")
+    #os.mkdir(package_dir)
 
     print("Creating README.txt from template")
     with open(setup_dir+"/README.txt", "w") as f:
@@ -65,15 +91,18 @@ def main():
     with open(setup_dir+"/setup.py", "w") as f:
         f.write(SETUP_CONTENT)
 
-    binaries_dir = os.path.abspath(installer_dir+"/../binaries_"+BITS+"/")
+    if BITS == "32bit":
+        binaries_dir = os.path.abspath(installer_dir+"/../binaries/")
+    else:
+        binaries_dir = os.path.abspath(installer_dir+"/../binaries_"+BITS+"/")
     print("Copying binaries to package dir")
-    ret = os.system("cp -rf "+binaries_dir+"/* "+package_dir)
-    assert ret == 0
+    shutil.copytree(binaries_dir, package_dir)
 
     os.chdir(package_dir)
-    print("Removing .log files from the package dir")
-    ret = os.system("rm *.log")
-    # assert ret == 0 - if there are no .log files this assert would fail.
+    print("Removing .log .pyc .pdb files from the package dir")
+    glob_remove("*.log")
+    glob_remove("*.pyc")
+    glob_remove("*.pdb")
     os.chdir(installer_dir)
 
     print("Creating __init__.py from template")
@@ -82,14 +111,6 @@ def main():
 
     print("Creating examples dir in package dir")
     os.mkdir(package_dir+"/examples/")
-
-    print("Moving kivy-select-boxes dir to examples dir")
-    shutil.move(package_dir+"/kivy-select-boxes",
-            package_dir+"/examples/kivy-select-boxes")
-
-    #kivy_select_boxes_dir = package_dir+"/examples/kivy-select-boxes/"
-    #ret = os.system("cp -rf "+kivy_select_boxes_dir+"/* "+package_dir+"/wx/")
-    #assert ret == 0
 
     print("Creating wx dir in package dir")
     os.mkdir(package_dir+"/wx/")
@@ -104,21 +125,18 @@ def main():
         if os.path.basename(example).startswith("__"):
             continue
         os.rename(example, package_dir+"/examples/"+os.path.basename(example))
-    ret = os.system("mv "+package_dir+"/*.html "+package_dir+"/examples/")
-    assert ret == 0
+    glob_move(package_dir+"/*.html", package_dir+"/examples/")
 
     print("Copying wx-subpackage to wx dir in package dir")
     wx_subpackage_dir = os.path.abspath(installer_dir+"/../../wx-subpackage/")
-    ret = os.system("cp -rf "+wx_subpackage_dir+"/* "+package_dir+"/wx/")
-    assert ret == 0
+    glob_copy(wx_subpackage_dir+"/*", package_dir+"/wx/")
 
     print("Moving wx examples from wx/examples to examples/wx")
-    shutil.move(package_dir+"/wx/examples", package_dir+"/wx/wx/")
-    shutil.move(package_dir+"/wx/wx/", package_dir+"/examples/")
+    glob_move(package_dir+"/wx/examples/*", package_dir+"/examples/wx/")
+    os.rmdir(package_dir+"/wx/examples/")
 
     print("Copying package dir examples to setup dir")
-    ret = os.system("cp -rf "+package_dir+"/examples/ "+setup_dir+"/examples/")
-    assert ret == 0
+    glob_copy(package_dir+"/examples/", setup_dir+"/examples/")
 
     print("Setup Package created.")
 
