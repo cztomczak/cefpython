017198f7dd641ef00f545ff5f241b4a0114143b1
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-07-28 16:11:55 +0200

    Added Qt example on Linux (Issue 88).
    Exposed some of the GTK API to WindowUtils.

--- cefpython/window_utils_linux.pyx
+++ cefpython/window_utils_linux.pyx
@@ -3,16 +3,23 @@
 # Website: http://code.google.com/p/cefpython/
 
 class WindowUtils:
-
     # You have to overwrite this class and provide implementations
     # for these methods.
 
     @staticmethod
     def GetParentHandle(WindowHandle windowHandle):
-        Debug("WindowUtils::GetParentHandle() not implemented!")
+        Debug("WindowUtils::GetParentHandle() not implemented (returns 0)")
         return 0
 
     @staticmethod
     def IsWindowHandle(WindowHandle windowHandle):
-        Debug("WindowUtils::IsWindowHandle() not implemented!")
+        Debug("WindowUtils::IsWindowHandle() not implemented (always True)")
         return True
+
+    @staticmethod
+    def gtk_plug_new(long long gdkNativeWindow):
+        return <long long>gtk_plug_new(<GdkNativeWindow>gdkNativeWindow)
+
+    @staticmethod
+    def gtk_widget_show(long long gtkWidgetPtr):
+        gtk_widget_show(<GtkWidget*>gtkWidgetPtr)
