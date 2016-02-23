9eaca827ce90a43a7394458527c234c18bd656c5
Czarek Tomczak <cztomczak@users.noreply.github.com>
2016-02-23 16:56:40 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,32 @@
 from recommonmark.parser import CommonMarkParser
 
-source_suffix = ['.rst', '.md']
+version = '31.2'
+release = '31.2'
+
+project = u'CEF Python'
+description = u'CEF Python Documentation'
+author = u'The CEF Python authors'
+copyright = u'2012, '+author
+
+master_doc = 'Home'
+source_suffix = ['.md']
 source_parsers = {
     '.md': CommonMarkParser,
 }
-project = u'CEF Python'
+
+pygments_style = 'sphinx'
 language = 'en'
-master_doc = 'Home'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+
+latex_documents = [
+    (master_doc, 'cefpython.tex', description,
+     author, 'manual'),
+]
 man_pages = [
-    (master_doc, 'cefpython', u'CEF Python Documentation',
-     [u'Czarek Tomczak'], 1)
+    (master_doc, 'cefpython', description,
+     [author], 1)
 ]
 htmlhelp_basename = 'cefdoc'
