0c8d6d2834560d2f18f97889c703f43a41601fc6
Czarek Tomczak <cztomczak@users.noreply.github.com>
2016-02-23 16:35:05 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,5 +4,10 @@ source_suffix = ['.rst', '.md']
 source_parsers = {
     '.md': CommonMarkParser,
 }
-
+project = u'CEF Python'
 master_doc = 'Home'
+man_pages = [
+    (master_doc, 'cefpython', u'CEF Python Documentation',
+     [u'Czarek Tomczak'], 1)
+]
+htmlhelp_basename = 'cefdoc'
