d94991fb23db2fc1cff094fae52d1a83c0c9a3c8
Czarek Tomczak <cztomczak@users.noreply.github.com>
2016-02-23 18:21:47 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,15 @@
-"""The build configuration file for Sphinx - Python Documentation Generator."""
+"""The build configuration file for Sphinx docs generator.
 
+To generate documentation locally run these commands:
+
+pip install sphinx sphinx-autobuild
+pip install recommonmark
+pip install sphinx_rtd_theme
+sphinx-build -b html -d _build/doctrees . _build/html
+"""
+
+import os
+import sys
 from recommonmark.parser import CommonMarkParser
 
 version = '31.2'
@@ -10,7 +20,7 @@ description = u'CEF Python Documentation'
 author = u'The CEF Python authors'
 copyright = u'2012, '+author
 
-master_doc = 'Home'
+master_doc = 'index'
 source_suffix = ['.rst', '.md']
 source_parsers = {
     '.md': CommonMarkParser,
@@ -32,3 +42,9 @@ man_pages = [
      [author], 1)
 ]
 htmlhelp_basename = 'cefdoc'
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
