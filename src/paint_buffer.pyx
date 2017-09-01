8a3a77a835c65a25692e0087fe98261f1d0f65f1
cztomczak <cztomczak@users.noreply.github.com>
2017-09-01 17:34:14 +0200

    Fix PaintBuffer.GetIntPointer missing method (code copied from master).

--- src/paint_buffer.pyx
+++ src/paint_buffer.pyx
@@ -18,16 +18,58 @@ cdef class PaintBuffer:
     cdef int height
     cdef Py_ssize_t length
 
-    def __init__(self):
-        # TODO: Remove deprecated methods below from API reference
-        #       and update examples during next release
-        self.GetIntPointer = self.GetPointer
-        self.GetString = self.GetBytes
-
     cpdef uintptr_t GetPointer(self) except *:
+        # BEFORE MODIFYING CODE:
+        # There is an exact copy of this method named "GetIntPointer"
+        # (deprecated).
         return <uintptr_t>self.buffer
 
     cpdef object GetBytes(self, str mode="bgra", str origin="top-left"):
+        # BEFORE MODIFYING CODE:
+        # There is an exact copy of this method named "GetString" (deprecated).
+        cdef void* dest
+        cdef py_bool dest_alloced = False
+        cdef object ret
+
+        origin = origin.lower()
+        mode = mode.lower()
+        assert origin in ("top-left", "bottom-left"), "Invalid origin"
+        assert mode in ("bgra", "rgba"), "Invalid mode"
+
+        # To get rid of a Cython warning:
+        # | '__pyx_v_dest' may be used uninitialized in this function
+        dest = <void*>malloc(0)
+
+        if mode == "rgba":
+            if not dest_alloced:
+                dest = <void*>malloc(self.length)
+                dest_alloced = True
+            SwapBufferFromBgraToRgba(dest, self.buffer, self.width,
+                    self.height)
+
+        if origin == "bottom-left":
+            if not dest_alloced:
+                dest = <void*>malloc(self.length)
+                dest_alloced = True
+            FlipBufferUpsideDown(dest, self.buffer, self.width, self.height)
+
+        if dest_alloced:
+            ret = (<char*>dest)[:self.length]
+            free(dest)
+            return ret
+        else:
+            return (<char*>self.buffer)[:self.length]
+
+    # ---- DEPRECATED ---------------------------------------------------------
+    # TODO: remove deprecated methods from API reference during next release.
+    # TODO: remove deprecated methods after users had time to update code.
+
+    cpdef uintptr_t GetIntPointer(self) except *:
+        """@deprecated."""
+        return <uintptr_t>self.buffer
+
+    cpdef object GetString(self, str mode="bgra", str origin="top-left"):
+        """@deprecated."""
         cdef void* dest
         cdef py_bool dest_alloced = False
         cdef object ret
