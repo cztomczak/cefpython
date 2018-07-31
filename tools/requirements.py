0867dbeff3882e5f766ea4cc706a1e3a441f414b
cztomczak <cztomczak@users.noreply.github.com>
2018-07-31 20:40:31 +0200

    Create tools/requirements.py

---
+++ tools/requirements.py
@@ -0,0 +1,22 @@
+"""
+Installs Python dependencies using the pip tool.
+See the requirements.txt file.
+pip install --upgrade -r ../tools/requirements.txt
+"""
+
+from common import *
+import subprocess
+
+
+def main():
+    args = []
+    if sys.executable.startswith("/usr/"):
+        args.append("sudo")
+    requirements = os.path.join(TOOLS_DIR, "requirements.txt")
+    args.extend(["pip", "install", "--upgrade", "-r", requirements])
+    retcode = subprocess.call(args)
+    sys.exit(retcode)
+
+
+if __name__ == "__main__":
+    main()
